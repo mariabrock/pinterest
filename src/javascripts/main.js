d6ee772b0a93d41bec118be261e067b5a29ba51f
mariabrock <mariabrock414@gmail.com>
2019-11-03 15:59:12 -0600

    auth wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,19 @@
+import firebase from 'firebase';
+
+import auth from './components/Auth/auth';
+import navbar from './components/Navbar/navbar';
+import apiKeys from './helpers/apiKeys.json';
+
 import 'bootstrap';
 import '../styles/main.scss';
 
 console.error('hi');
 
-// const init = () => {
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  auth.loginButton();
+  // authData.checkLoginStatus();
+  navbar.logoutEvent();
+};
 
-// };
+init();
