ad6d7c1a5a58ea17080f73146811d3e70e606629
mariabrock <mariabrock414@gmail.com>
2019-11-05 21:20:38 -0600

    added boards on login

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 
 const authDiv = $('#auth');
 const homeDiv = $('#home');
-// const boardsDiv = $('#boards');
+const boardsDiv = $('#boards');
 const logoutNavbar = $('#navbar-button-logout');
 
 const checkLoginStatus = () => {
@@ -14,14 +14,14 @@ const checkLoginStatus = () => {
       logoutNavbar.removeClass('d-none');
       authDiv.addClass('d-none');
       homeDiv.addClass('d-none');
-      // boardsDiv.removeClass('d-none');
+      boardsDiv.removeClass('d-none');
       // console.error(user.uid);
     } else {
       //  nobody logged in SHOW auth component
       logoutNavbar.addClass('d-none');
       authDiv.removeClass('d-none');
       homeDiv.removeClass('d-none');
-      // boardsDiv.addClass('d-none');
+      boardsDiv.addClass('d-none');
     }
   });
 };
