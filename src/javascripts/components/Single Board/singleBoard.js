db3cef1b351757267006fb9fc357df0c2376b838
mariabrock <mariabrock414@gmail.com>
2019-11-06 20:44:20 -0600

    still wip

--- src/javascripts/components/Single Board/singleBoard.js
+++ src/javascripts/components/Single Board/singleBoard.js
@@ -1,9 +1,17 @@
-import utilities from '../../helpers/utilities';
+// import $ from 'jquery';
+// import utilities from '../../helpers/utilities';
 
-// const getCurrentUid = () => firebase.auth().currentUser.uid;
+// // const getCurrentUid = () => firebase.auth().currentUser.uid;
 
-const printSingleBoard = () => {
-  utilities.printToDom.printSingleBoard();
-};
+// const printSingleBoard = () => {
+//   let domstring = '';
+//   let domString += '<div class="card" style="col-3">';
+//   let domString += `<img src="${board1.pin1.imageUrl}" class="card-img-top">`;
+//   let domString += '<div class="card-body">';
+//   let domString += `<h5 class="card-title">${board1.name}</h5>`;
+//   let domString += '<button class="btn btn-primary">View</button>';
+//   let domString += '</div></div>';
+//   utilities.printToDom.printSingleBoard();
+// };
 
-export default { printSingleBoard };
+// export default { printSingleBoard };
