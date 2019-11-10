e20403ee9e7b024bf0b47ee781830c739af54163
mariabrock <mariabrock414@gmail.com>
2019-11-10 12:51:44 -0600

    MAH BUTTONS WORK BOI

--- src/javascripts/components/Boards/boards.js
+++ src/javascripts/components/Boards/boards.js
@@ -3,11 +3,13 @@ import boardsData from '../../helpers/data/boardsData';
 import singleBoard from '../singleBoard/singleBoard';
 
 import utilities from '../../helpers/utilities';
+
 import './boards.scss';
 
 const addBoardClickEvent = (e) => {
   e.preventDefault();
   singleBoard.showOneBoard(e.target.id);
+  console.log('addBoardClickEvent');
 };
 
 // <img src="..." class="card-img-top" alt="...">
@@ -19,11 +21,11 @@ const printAllBoards = (user) => {
       domString += '<div class="row" id="board-cards">';
       bords.forEach((bord) => {
         domString += `
-          <div class="col-sm-6">
+          <div class="col-sm-4">
             <div class="card">
               <div class="card-body">
               <h5 class="card-title">${bord.boardName}</h5>
-              <button class="btn btn-primary" id="view-board">View</button>
+              <button class="btn btn-primary boardClick" id="${bord.boardId}">View</button>
               </div>
             </div>
           </div>  
@@ -31,9 +33,9 @@ const printAllBoards = (user) => {
       });
       domString += '</div>';
       utilities.printToDom('boards', domString);
-      $('body').on('click', '.myBoard', addBoardClickEvent);
+      $('body').on('click', '.boardClick', addBoardClickEvent);
     })
     .catch((error) => console.error(error));
 };
 
-export default { printAllBoards };
+export default { printAllBoards, addBoardClickEvent };
